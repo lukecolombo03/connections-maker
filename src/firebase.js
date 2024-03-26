5d010ee4b9191030b69a9ea2204447fb86ae261e
Zachary G <zachgerst@gmail.com>
2024-03-26 12:35:24 -0500

    added firebase hosting. All pre-existing files moved to src.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANFYM1l3tTscSL-umOzmHuU4GyQQ5Aa3M",
+  authDomain: "connector-ff2ae.firebaseapp.com",
+  projectId: "connector-ff2ae",
+  storageBucket: "connector-ff2ae.appspot.com",
+  messagingSenderId: "23700257108",
+  appId: "1:23700257108:web:b58b77de41d110532630c1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
